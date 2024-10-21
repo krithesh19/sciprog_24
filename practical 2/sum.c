e835decdf22ff13af4162fa0696d0e4838d6a588
KRITHESH <121800781+krithesh19@users.noreply.github.com>
2024-10-21 18:40:32 +0100

    Update sum.c

--- practical 2/sum.c
+++ practical 2/sum.c
@@ -10,7 +10,7 @@ int main(void) {
 /* First sum */
    sum1 = 0.0;
    for (i=1; i<=1000; i++) {
-      /*  Insert here */
+      sum1 sum1+1.0/(float)i;
    }
 
 
@@ -18,13 +18,14 @@ int main(void) {
    sum2 = 0.0;
    for (i=1000; i>0; i--) {
       /* Insert the same line as above except use sum2 */
+      sum2 = sum2+1.0/(float)i;
    }
 
    printf(" Sum1=%f\n",sum1);
    printf(" Sum2=%f\n",sum2);
 
 /* Find the difference */
-   diff = /* ?? */
+   diff = sum1-sum2;
 
    printf(" Difference between the two is %f\n",diff);
 
